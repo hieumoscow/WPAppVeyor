45cdf4778269cf89012a7ce9580e2f0e48670811
hieumoscow <hieumoscow89@yahoo.com>
2015-08-24 17:33:35 +0700

    WP

---
+++ WPAppVeyor.Core/WPAppVeyor.WP/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WPAppVeyor.WP.Resources;
+
+namespace WPAppVeyor.WP
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ WPAppVeyor.Core/WPAppVeyor.WP/Views/FirstView.xaml.cs
@@ -0,0 +1,12 @@
+using Cirrious.MvvmCross.WindowsPhone.Views;
+
+namespace WPAppVeyor.WP.Views
+{
+    public partial class FirstView : MvxPhonePage
+    {
+        public FirstView()
+        {
+            InitializeComponent();
+        }
+    }
+}
